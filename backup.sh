8a00eea1197c27644882427d0fc798a7b8a1d809
Peter Oslington <peteroslington1@gmail.com>
2021-07-31 16:02:12 +1000

    Added basic functionality for making and removing backups

--- backup.sh
+++ backup.sh
@@ -1,1 +1,29 @@
-echo "Hello world!"
+echo "\nRunning backup script at $(date)..."
+
+# if appropriate setup present, run backup
+if [ $MYSQL_USER ] && [ $MYSQL_PASSWORD ] && [ $MYSQL_DATABASE ]
+then
+    mysqldump --single-transaction -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > /backups/nextcloud-sqlbkp_`date +"%Y%m%d"`.bak
+else
+    echo "Please set 'MYSQL_USER', 'MYSQL_PASSWORD' and 'MYSQL_DATABASE' variables to continue."
+    exit
+fi
+
+# test which backups should be removed
+if [ $N_BACKUPS ]
+then
+    FILES_TO_DELETE=$(ls -Art ~/test/* | head -n-$N_BACKUPS)
+else
+    echo "'N_BACKUPS not set. Keeping all backups."
+fi
+
+
+# remove appropriate backups
+if [ $FILES_TO_DELETE ]
+then
+    echo "Maximum number of backup files reached. Removing:"
+    echo "$FILES_TO_DELETE"
+    rm $FILES_TO_DELETE
+else
+    echo "No backups were removed."
+fi
